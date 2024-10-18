afdfb783debc3869b3bd631b8ea1e77ed4310155
thomashollingsworth <123562498+thomashollingsworth@users.noreply.github.com>
2024-10-18 15:49:07 +0100

    Structuring Files

--- LatticeClass.py
+++ LatticeClass.py
@@ -1,3 +1,6 @@
+"""This file is exclusively used to store the Lattice Class
+Any other code i.e. testing debugging must be kept under the, if __name__=="__main__":"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.random as random
@@ -538,60 +541,59 @@ class Lattice:
             return pickle.load(f)
 
 
-"""-----------------------------------------------------------------------------------------
+if __name__ == "__main__":
 
-This is some example use of the Lattice class"""
+    """-----------------------------------------------------------------------------------------
 
+    This is some example use of the Lattice class"""
 
-newgrid = Lattice(
-    5, 5
-)  # Generates a lattice with a certain size, all other parameters could also be explicitly specified here
-# e.g. newgrid.beta=1, newgrid.a1=10
+    newgrid = Lattice(
+        5, 5
+    )  # Generates a lattice with a certain size, all other parameters could also be explicitly specified here
+    # e.g. newgrid.beta=1, newgrid.a1=10
 
-interval = 1000
-iterations = 500000
+    interval = 1000
+    iterations = 500000
 
-# Creates empty arrays to store quantites of interest
-energy_array = np.zeros(iterations)[::interval]
-phi_std_array = np.zeros(iterations)[::interval]
+    # Creates empty arrays to store quantites of interest
+    energy_array = np.zeros(iterations)[::interval]
+    phi_std_array = np.zeros(iterations)[::interval]
 
+    for i in range(iterations):
 
-for i in range(iterations):
+        # Could edit any of the parameters during the iteration process
+        # e.g. could dynamically alter beta using a Simulated Annealing algorithm
+        # (newgrid.beta = 0.0001 + 0.2069 * np.log(1 + i))
 
-    # Could edit any of the parameters during the iteration process
-    # e.g. could dynamically alter beta using a Simulated Annealing algorithm
-    # (newgrid.beta = 0.0001 + 0.2069 * np.log(1 + i))
+        newgrid.make_update()  # repetitively updating the lattice
 
-    newgrid.make_update()  # repetitively updating the lattice
+        if (
+            i % interval == 0
+        ):  # periodically recording the energy changes/any other quantities of interest
+            newgrid.update_stats()  # have to explicitly call update_stats() isn't included in make_update()
+            energy_array[i // interval] = newgrid.energy_count
+            phi_std_array[i // interval] = newgrid.phi_std
+            print(f"Iteration {i} Completed")
 
-    if (
-        i % interval == 0
-    ):  # periodically recording the energy changes/any other quantities of interest
-        newgrid.update_stats()  # have to explicitly call update_stats() isn't included in make_update()
-        energy_array[i // interval] = newgrid.energy_count
-        phi_std_array[i // interval] = newgrid.phi_std
-        print(f"Iteration {i} Completed")
+    # Plotting results
 
+    newgrid.draw_fields(save=False)
+    # print(f"Mean h:{newgrid.h_mean}\nMean phi:{newgrid.phi_mean}\nStd. Phi: {newgrid.phi_std}")
 
-# Plotting results
+    fig, ax1 = plt.subplots()
 
-newgrid.draw_fields(save=False)
-# print(f"Mean h:{newgrid.h_mean}\nMean phi:{newgrid.phi_mean}\nStd. Phi: {newgrid.phi_std}")
+    color1 = "tab:blue"
+    ax1.set_xlabel("Iterations")
+    ax1.set_ylabel("Energy", color=color1)
+    ax1.plot(np.arange(iterations)[::interval], energy_array, color=color1)
+    ax1.tick_params(axis="y", labelcolor=color1)
 
-fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
 
-color1 = "tab:blue"
-ax1.set_xlabel("Iterations")
-ax1.set_ylabel("Energy", color=color1)
-ax1.plot(np.arange(iterations)[::interval], energy_array, color=color1)
-ax1.tick_params(axis="y", labelcolor=color1)
+    color2 = "tab:green"
+    ax2.set_ylabel("Deviation of phi", color=color2)
+    ax2.plot(np.arange(iterations)[::interval], phi_std_array, color=color2)
+    ax2.tick_params(axis="y", labelcolor=color2)
 
-ax2 = ax1.twinx()
-
-color2 = "tab:green"
-ax2.set_ylabel("Deviation of phi", color=color2)
-ax2.plot(np.arange(iterations)[::interval], phi_std_array, color=color2)
-ax2.tick_params(axis="y", labelcolor=color2)
-
-fig.suptitle("Total energy and Deviation of Phi Disturbution")
-plt.show()
+    fig.suptitle("Total energy and Deviation of Phi Disturbution")
+    plt.show()
---
+++ Standard_Imports.py
@@ -0,0 +1,11 @@
+from LatticeClass import Lattice
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.random as random
+
+# Used for saving data
+import csv
+import datetime
+import pickle
+
+print("Hello")
